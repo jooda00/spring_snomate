6e8bbbb831cd79650099141d2baf9d87f598484f
soluxsolux <dragon2000a@naver.com>
2021-08-11 22:25:44 +0900

    [1] user 모델, 레포 추가합니다.

---
+++ snomate/src/main/java/com/example/snomate/model/User.java
@@ -0,0 +1,36 @@
+package com.example.snomate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "snomate_user")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name = "user_email")
+	private String user_email;
+	
+	@Column(name = "user_password")
+	private String user_password;
+	
+	@Column(name = "user_name")
+	private String user_name;
+	
+	
+	@Column(name = "temperature")
+	private Float temperature;
+}
---
+++ snomate/src/main/java/com/example/snomate/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.snomate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.snomate.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}

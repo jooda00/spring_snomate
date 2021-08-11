3db5008b36af0584f01cf02bb75762044a48472a
soluxsolux <dragon2000a@naver.com>
2021-08-11 23:21:13 +0900

    [1] model 다시 커밋합니다.

--- snomate/src/main/java/com/example/snomate/model/Project.java
+++ snomate/src/main/java/com/example/snomate/model/Project.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class Project {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer pj_id;
 	
 	@Column(name = "title")
--- snomate/src/main/java/com/example/snomate/model/User.java
+++ snomate/src/main/java/com/example/snomate/model/User.java
@@ -3,6 +3,7 @@ package com.example.snomate.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column(name = "user_email")
@@ -30,6 +31,8 @@ public class User {
 	@Column(name = "user_name")
 	private String user_name;
 	
+	@Column(name = "test")
+	private String test;
 	
 	@Column(name = "temperature")
 	private Float temperature;
